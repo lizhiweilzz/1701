c47123d17a70c11ff1b7048dd7361a38cebf0b7e
李志威 <@192.168.0.141>
2018-01-15 15:46:03 +0800

    this

--- SSM_Chapter10_example8/src/cn/smbms/pojo/User.java
+++ SSM_Chapter10_example8/src/cn/smbms/pojo/User.java
@@ -131,13 +131,5 @@ public class User {
 		this.modifyDate = modifyDate;
 	}
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", userCode=" + userCode + ", userName=" + userName + ", userPassword=" + userPassword
-				+ ", gender=" + gender + ", birthday=" + birthday + ", phone=" + phone + ", address=" + address
-				+ ", userRole=" + userRole + ", createdBy=" + createdBy + ", creationDate=" + creationDate
-				+ ", modifyBy=" + modifyBy + ", modifyDate=" + modifyDate + ", age=" + age + ", userRoleName="
-				+ userRoleName + "]";
-	}
 	
 }
